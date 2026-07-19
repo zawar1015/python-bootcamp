2e8c0dd404f3f86182954726cb9066042a6c9387
^Zawar Hussain <zawarhussin.malak@gmial.com>
2026-07-19 17:17:23 +0500

    initial commit

---
+++ studentportfolio.py
@@ -0,0 +1,32 @@
+# Student Profile Program
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# university = input("Enter your university: ")
+# department = input("Enter your department: ")
+# cgpa = float(input("Enter your CGPA: "))
+# fav_language = input("Enter your favorite programming language: ")
+
+# print("\n" + "=" * 40)
+# print("         STUDENT PROFILE")
+# print("=" * 40)
+# print(f"Name                    : {name}")
+# print(f"Age                     : {age}")
+# print(f"University              : {university}")
+# print(f"Department              : {department}")
+# print(f"CGPA                    : {cgpa:.2f}")
+# print(f"Favorite Programming Language : {fav_language}")
+# print("=" * 40)
+
+# print("\n" + "="*40)
+# print("             STUDENT PROFILE ")
+# print(f"Name                           : {name}")
+# print(f"Age                            : {age}")
+# print(f"University                     : {university}")
+# print(f"Department                     : {department}")
+# print(f"CGPA                           : {cgpa}")
+# print(f"Favourite P_language           : {fav_language}")
+# print(f"="* 40)
+
+# student_card.py
+
